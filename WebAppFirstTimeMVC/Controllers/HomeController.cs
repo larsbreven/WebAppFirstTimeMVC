c6347774a37b0da99943eaf2af02b384d9a6f689
Lars Brevén <lars.breven@telia.com>
2021-04-12 18:34:35 +0200

    Add project files.

---
+++ WebAppFirstTimeMVC/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppFirstTimeMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        static List<string[]> contactList = new List<string[]>();
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Experiment()
+        {
+            return View();
+        }
+
+        [HttpGet]//only client get request can call this one
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ContactUs(string name, string email, string message)
+        {
+            contactList.Add(new string[]{name, email, message});
+
+            ViewBag.ContactList = contactList;
+
+            return View("ContactList");
+        }
+    }
+}
